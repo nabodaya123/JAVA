e5b305ca02feacbe8937c21d7b27f97663284050
nabodaya123 <sahunabodaya@gmail.com>
2026-09-10 13:57:01 +0000

    calculate the average of three numbers

---
+++ basic/Average.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class average {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        float a= sc.nextFloat();
+        float b=sc.nextFloat();
+        float c= sc.nextFloat();
+        float sum=(a+b+c)/3;
+        System.out.println(sum);
+    }
+    
+}

534f6c5d822e7744d403ab07645786dbd57cb216
nabodaya123 <sahunabodaya@gmail.com>
2026-09-13 20:11:01 +0000

    Calculate the two product

---
+++ basic/product.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class product {
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the value a:");
+        int a=sc.nextInt();
+        System.out.print("Enter the value b:");
+        int b=sc.nextInt();
+        System.out.print("Calculate the two product is:");
+        int product=a*b;
+        System.out.println(product);
+        
+
+    }
+    
+}

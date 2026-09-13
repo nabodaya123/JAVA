b7e986152e4f78ec5d9f6cbb7c964f2d6e30befe
nabodaya123 <sahunabodaya@gmail.com>
2026-09-13 20:17:58 +0000

    Two variable sum Calculate

---
+++ basic/sum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class sum {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a value:");
+        int a=sc.nextInt();
+        System.out.print("Enter b value:");
+        int b= sc.nextInt();
+        System.out.print("Calculate the sum:");
+        int sum=a+b;
+        System.out.println(sum);
+
+    }
+    
+}

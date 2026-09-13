839660749ac6fea9bf3f4fc31d236577ba816561
nabodaya123 <sahunabodaya@gmail.com>
2026-09-13 20:15:35 +0000

    Calculate the area of a Square

---
+++ basic/Square_Area.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Square_Area {
+    public void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the value of side:");
+        float side= sc .nextFloat();
+        System.out.print("Enter the calculate value:");
+        float area=3.14f*side*side;
+        System.out.println(area);
+
+    }
+    
+}

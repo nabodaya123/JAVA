a1d9712d06ee7ae50619ef9fab4e1080a9f8c2f9
nabodaya123 <sahunabodaya@gmail.com>
2026-09-15 07:34:07 +0000

    It is leet code #4 question but it is brute force Approach.But next it implementbest Approach

---
+++ Array/duplicate_brute.java
@@ -0,0 +1,16 @@
+public class duplicate_brute{
+    public static boolean duplicate(int nums[]){
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String args[]){
+        int nums[]={1,2,3,4,1};
+        System.out.println(duplicate(nums));
+    }
+}

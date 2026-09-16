754ac50799ff394db4180eabd1aee83b0f6a2efc
nabodaya123 <sahunabodaya@gmail.com>
2026-09-16 18:20:12 +0000

    calculate the average using function

---
+++ Function/average.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class average {
+    public static int average( int a, int b, int c){
+        int ave =(a+b+c)/3;
+        return ave;
+        // System.out.println(ave);
+    }
+
+    public static void main(String args[]){
+       System.out.println( average(7,9,8));
+    }
+    
+}
+
+
